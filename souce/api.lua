0a37645cea83ac8f94840c9b33c65c17c21411b0
d <66913721+catvskid@users.noreply.github.com>
2021-11-30 15:45:29 -0300

    Create api.lua

---
+++ souce/api.lua
@@ -0,0 +1,8 @@
+{
+	"latestVersion": "v2.0.3",
+	"sourceCodeURL": "https://pastebin.com/raw/w2S8YyDt",
+	"motd": {
+		"title": "v2.0.3 Change Notes",
+		"body": "* Fixed settings not properly applying-- AKA 'the aimbot not working'. Sorry about that!\n+ Added custom camera sensitivity support for Da Hood\n\nJoin discord.me/temporal for updates!"
+	}
+}
