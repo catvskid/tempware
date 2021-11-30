218cc730f00e39d330af5361934a05de739d0abf
d <66913721+catvskid@users.noreply.github.com>
2021-11-30 15:50:40 -0300

    Update fish.lua

--- souce/fish.lua
+++ souce/fish.lua
@@ -16,7 +16,7 @@
 	Have fun!
 ]]
 
-local API_URL = "https://pastebin.com/raw/tKQBLkNJ"
+local API_URL = "https://github.com/catvskid/visualsnaimbot/blob/main/souce/api.js"
 local DATA = game:GetService("HttpService"):JSONDecode(game:HttpGet(API_URL))
 
 local ver = "v2.0.3"
