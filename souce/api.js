e07ad12f3bff1aa43d716b3c59f544cbea64aa8f
d <66913721+catvskid@users.noreply.github.com>
2021-11-30 15:59:23 -0300

    Update api.js

--- souce/api.js
+++ souce/api.js
@@ -1,6 +1,6 @@
 {
 	"latestVersion": "v2.0.3",
-	"sourceCodeURL": "https://raw.githubusercontent.com/catvskid/visualsnaimbot/main/souce/fish.lua?token=AP6QLOPDFKT64EDILY5HCHTBUZZL6",
+	"sourceCodeURL": "https://raw.githubusercontent.com/catvskid/visualsnaimbot/main/souce/fish.lua",
 	"motd": {
 		"title": "v2.0.3 Change Notes",
 		"body": "* Fixed settings not properly applying-- AKA 'the aimbot not working'. Sorry about that!\n+ Added custom camera sensitivity support for Da Hood\n\nJoin discord.me/temporal for updates!"
